ed34b92ad81a38677299567db9c0094b6311d9f9
Hank Kung <hank910140@gmail.com>
2019-12-23 04:51:40 +0800

    eval

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,57 @@
 import numpy as np
-
+import torch
 
 class Evaluator(object):
+    def __init__(self, num_class):
+        self.num_class = num_class
+        self.confusion_matrix = torch.zeros((self.num_class,)*2)
+
+    def Pixel_Accuracy(self):
+        Acc = torch.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
+        return Acc
+
+    def Pixel_Accuracy_Class(self):
+        Acc = torch.diag(self.confusion_matrix) / self.confusion_matrix.sum(axis=1)
+        Acc = self.torch_nanmean(Acc)
+        return Acc
+
+    def Mean_Intersection_over_Union(self):
+        MIoU = torch.diag(self.confusion_matrix) / (
+                    torch.sum(self.confusion_matrix, axis=1) + torch.sum(self.confusion_matrix, axis=0) -
+                    torch.diag(self.confusion_matrix))
+        MIoU = self.torch_nanmean(MIoU)
+        return MIoU
+
+    def Frequency_Weighted_Intersection_over_Union(self):
+        freq = torch.sum(self.confusion_matrix, axis=1) / torch.sum(self.confusion_matrix)
+        iu = torch.diag(self.confusion_matrix) / (
+                    torch.sum(self.confusion_matrix, axis=1) + torch.sum(self.confusion_matrix, axis=0) -
+                    torch.diag(self.confusion_matrix))
+
+        FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+        return FWIoU
+
+    def _generate_matrix(self, gt_image, pre_image):
+        mask = (gt_image >= 0) & (gt_image < self.num_class)
+        label = self.num_class * gt_image[mask].int() + pre_image[mask]
+        count = torch.bincount(label, minlength=self.num_class**2)
+        confusion_matrix = count.reshape(self.num_class, self.num_class)
+        return confusion_matrix
+
+    def add_batch(self, gt_image, pre_image):
+        assert gt_image.shape == pre_image.shape
+        self.confusion_matrix += self._generate_matrix(gt_image, pre_image)
+
+    def reset(self):
+        self.confusion_matrix = torch.zeros((self.num_class,) * 2)
+
+
+    def torch_nanmean(x):
+        num = torch.where(torch.isnan(x), torch.full_like(x, 0), torch.full_like(x, 1)).sum()
+        value = torch.where(torch.isnan(x), torch.full_like(x, 0), x).sum()
+        return value / num
+
+class Evaluator_cpu(object):
     def __init__(self, num_class):
         self.num_class = num_class
         self.confusion_matrix = np.zeros((self.num_class,)*2)
@@ -44,7 +94,4 @@ class Evaluator(object):
 
     def reset(self):
         self.confusion_matrix = np.zeros((self.num_class,) * 2)
-
-
-
 
