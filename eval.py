ff4f0cebe154abb628ea211913cbfb865f0b0640
Hank Kung <hank910140@gmail.com>
2020-01-20 07:56:29 +0800

    update eval

--- eval.py
+++ eval.py
@@ -10,11 +10,11 @@ from utils.saver import Saver
 from utils.summaries import TensorboardSummary
 from utils.metrics import Evaluator
 from utils.eval_utils import AverageMeter
-from modeling.new_model import *
+from modeling.dense_model import *
 from torchviz import make_dot, make_dot_from_trace
 import time
 
-class trainNew(object):
+class Evaluation(object):
     def __init__(self, args):
         self.args = args
 
@@ -28,18 +28,17 @@ class trainNew(object):
         kwargs = {'num_workers': args.workers, 'pin_memory': True, 'drop_last': True}
         _, self.val_loader, self.test_loader, self.nclass = make_data_loader(args, **kwargs)
 
-        cell_path_d = os.path.join(args.saved_arch_path, 'genotype_device.npy')
-        cell_path_c = os.path.join(args.saved_arch_path, 'genotype_cloud.npy')
+        cell_path_1 = os.path.join(args.saved_arch_path, 'genotype_1.npy')
+        cell_path_2 = os.path.join(args.saved_arch_path, 'genotype_2.npy')
         network_path_space = os.path.join(args.saved_arch_path, 'network_path_space.npy')
 
-        new_cell_arch_d = np.load(cell_path_d)
-        new_cell_arch_c = np.load(cell_path_c)
+        new_cell_arch_1 = np.load(cell_path_1)
+        new_cell_arch_2 = np.load(cell_path_2)
  
         new_network_arch = np.load(network_path_space)
         
-        if args.network == 'dist':
+        if args.network == 'searched_arch':
             new_network_arch = [0, 1, 2, 2, 3, 2, 2, 1, 2, 1, 1, 2]
-            B_d=4
 
         elif args.network == 'autodeeplab':
             new_network_arch = [0, 0, 0, 1, 2, 1, 2, 2, 3, 3, 2, 1]
@@ -55,22 +54,17 @@ class trainNew(object):
             cell[8] = [19, 7]
             cell[9] = [18, 5]
             cell=np.int_(cell)
-            new_cell_arch_d = cell
-            new_cell_arch_c = cell      
-            B_d=5
+            new_cell_arch_1 = cell
+            new_cell_arch_2 = cell      
 
         # Define network
-        model = new_cloud_Model(network_arch= new_network_arch,
-                         cell_arch_d = new_cell_arch_d,
-                         cell_arch_c = new_cell_arch_c,
+        model = Model_2(network_arch= new_network_arch,
+                         cell_arch_d = new_cell_arch_1,
+                         cell_arch_c = new_cell_arch_2,
                          num_classes=self.nclass,
                          device_num_layers=6,
-                         B_d=B_d,
                          sync_bn=args.sync_bn)
 
-
-        # Define Criterion
-        # whether to use class balanced weights
         if args.use_balanced_weights:
             classes_weights_path = os.path.join(Path.db_root_dir(args.dataset), args.dataset + '_classes_weights.npy')
             if os.path.isfile(classes_weights_path):
@@ -80,12 +74,13 @@ class trainNew(object):
             weight = torch.from_numpy(weight.astype(np.float32))
         else:
             weight = None
+
         self.criterion = SegmentationLosses(weight=weight, cuda=args.cuda).build_loss(mode=args.loss_type)
         self.model, self.optimizer = model, optimizer
 
         # Define Evaluator
-        self.evaluator_device = Evaluator(self.nclass)
-        self.evaluator_cloud = Evaluator(self.nclass)
+        self.evaluator_1 = Evaluator(self.nclass)
+        self.evaluator_2 = Evaluator(self.nclass)
 
         # Using cuda
         if args.cuda:
@@ -123,14 +118,14 @@ class trainNew(object):
 
     def validation(self):
         self.model.eval()
-        self.evaluator_device.reset()
-        self.evaluator_cloud.reset()
+        self.evaluator_1.reset()
+        self.evaluator_2.reset()
         tbar = tqdm(self.val_loader, desc='\r')
         test_loss = 0.0
-        time_meter_d = AverageMeter()
-        time_meter_c = AverageMeter()
-        pred_d_meter = AverageMeter()
-        pred_c_meter = AverageMeter()
+        time_meter_1 = AverageMeter()
+        time_meter_2 = AverageMeter()
+        pred_1_meter = AverageMeter()
+        pred_2_meter = AverageMeter()
 
         for i, sample in enumerate(tbar):
             image, target = sample['image'], sample['label']
@@ -138,11 +133,11 @@ class trainNew(object):
                 image, target = image.cuda(), target.cuda()
 
             with torch.no_grad():
-                device_output, cloud_output, time_1, time_2 = self.model(image, evaluation=True)
+                output_1, output_2, time_1, time_2 = self.model(image, evaluation=True)
 
-            device_loss = self.criterion(device_output, target)
-            cloud_loss = self.criterion(cloud_output, target)
-            loss = (device_loss + cloud_loss) /2
+            loss_1 = self.criterion(output_1, target)
+            loss_2 = self.criterion(output_2, target)
+            loss = (loss_1 + loss_2) /2
             test_loss += loss.item()
 
             tbar.set_description('Test loss: %.3f' % (test_loss / (i + 1)))
@@ -150,54 +145,63 @@ class trainNew(object):
             torch.cuda.synchronize()
             tic = time.perf_counter()
 
-            pred_d = device_output.data.cpu().numpy()
-            pred_d = np.argmax(pred_d, axis=1)
+            pred_1 = output_1.data.cpu().numpy()
+            pred_1 = np.argmax(pred_1, axis=1)
+
             torch.cuda.synchronize()
-            pred_d_meter.update(tic - time.perf_counter())
+            pred_1_meter.update(tic - time.perf_counter())
 
             torch.cuda.synchronize()
             tic = time.perf_counter()
-            pred_c = cloud_output.data.cpu().numpy()
-            pred_c = np.argmax(pred_c, axis=1)
+
+            pred_2 = cloud_output.data.cpu().numpy()
+            pred_2 = np.argmax(pred_2, axis=1)
             torch.cuda.synchronize()
-            pred_c_meter.update(tic - time.perf_counter())
+            pred_2_meter.update(tic - time.perf_counter())
 
             target_show = target
             target = target.cpu().numpy()
 
             # Add batch sample into evaluator
-            self.evaluator_device.add_batch(target, pred_d)
-            self.evaluator_cloud.add_batch(target, pred_c)
-            self.summary.visualize_image(self.writer, self.args.dataset, image, target_show, cloud_output, i)
-            time_meter_d.update(time_1)
-            time_meter_c.update(time_2)
+            self.evaluator_1.add_batch(target, pred_1)
+            self.evaluator_2.add_batch(target, pred_2)
+            self.summary.visualize_image(self.writer, self.args.dataset, image, target_show, output_1, i)
+            time_meter_1.update(time_1)
+            time_meter_2.update(time_2)
 
-        mIoU_d = self.evaluator_device.Mean_Intersection_over_Union()
-        mIoU_c = self.evaluator_cloud.Mean_Intersection_over_Union()
+        mIoU_1 = self.evaluator_1.Mean_Intersection_over_Union()
+        mIoU_2 = self.evaluator_2.Mean_Intersection_over_Union()
 
-        self.writer.add_scalar('val/device/mIoU', mIoU_d, 0)
-        self.writer.add_scalar('val/cloud/mIoU', mIoU_c, 0)
+        self.writer.add_scalar('val/1/mIoU', mIoU_1, 0)
+        self.writer.add_scalar('val/2/mIoU', mIoU_2, 0)
 
         print('Validation:')
-        print("device_mIoU:{}, cloud_mIoU: {}".format(mIoU_d, mIoU_c))
+        print("device_mIoU:{}, cloud_mIoU: {}".format(mIoU_1, mIoU_2))
         print('Loss: %.3f' % test_loss)
-        print("device_inference_time:{}, cloud_inference_time: {}".format(time_meter_d.average(), time_meter_c.average()))
-        print("device_pred_time:{}, cloud_pred_time: {}".format(pred_d_meter.average(), pred_c_meter.average()))
+        print("device_inference_time:{}, cloud_inference_time: {}".format(time_meter_1.average(), time_meter_2.average()))
+        print("device_pred_time:{}, cloud_pred_time: {}".format(pred_1_meter.average(), pred_2_meter.average()))
 
 def main():
-    parser = argparse.ArgumentParser(description="PyTorch DeeplabV3Plus Training")
-    parser.add_argument('--network', type=str, default='dist',
-                        choices=['dist', 'autodeeplab'],
-                        help='backbone name (default: resnet)')
+    
+    """ model setting """
+    parser.add_argument('--network', type=str, default='searched_dense',
+                        choices=['searched_dense', 'searched_baseline', 'autodeeplab'])
+    parser.add_argument('--num_model_1_layers', type=int, default=6)
+    parser.add_argument('--F_2', type=int, default=20)
+    parser.add_argument('--F_1', type=int, default=20)
+    parser.add_argument('--B_2', type=int, default=5)
+    parser.add_argument('--B_1', type=int, default=5)
+
+
+    """ dataset config"""
     parser.add_argument('--dataset', type=str, default='cityscapes',
                         choices=['pascal', 'coco', 'cityscapes'],
                         help='dataset name (default: pascal)')
-    parser.add_argument('--workers', type=int, default=4,
+    parser.add_argument('--workers', type=int, default=1,
                         metavar='N', help='dataloader threads')
-    parser.add_argument('--crop_size', type=int, default=320,
-                        help='crop image size')
-    parser.add_argument('--resize', type=int, default=512,
-                        help='resize image size')
+
+
+    """ training config """
     parser.add_argument('--sync-bn', type=bool, default=None,
                         help='whether to use sync bn (default: auto)')
     parser.add_argument('--freeze-bn', type=bool, default=False,
@@ -205,37 +209,31 @@ def main():
     parser.add_argument('--loss-type', type=str, default='ce',
                         choices=['ce', 'focal'],
                         help='loss func type (default: ce)')
-    # training hyper params
-    parser.add_argument('--epochs', type=int, default=None, metavar='N',
-                        help='number of epochs to train (default: auto)')
-    parser.add_argument('--start_epoch', type=int, default=0,
-                        metavar='N', help='start epochs (default:0)')
-    parser.add_argument('--test-batch-size', type=int, default=1,
-                        metavar='N', help='input batch size for \
-                                testing (default: auto)')
-    parser.add_argument('--use-balanced-weights', action='store_true', default=False,
-                        help='whether to use balanced weights (default: False)')
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N')
+    parser.add_argument('--test-batch-size', type=int, default=1, metavar='N')
+    parser.add_argument('--use-balanced-weights', action='store_true', default=False)
+
 
     parser.add_argument('--clean-module', type=int, default=0)
-    # cuda, seed and logging
-    parser.add_argument('--no-cuda', action='store_true', default=
-                        False, help='disables CUDA training')
+
+
+    """ cuda, seed and logging """
+    parser.add_argument('--no-cuda', action='store_true', default=False)
     parser.add_argument('--gpu-ids', type=str, default='0',
                         help='use which gpu to train, must be a \
                         comma-separated list of integers only (default=0)')
-    parser.add_argument('--seed', type=int, default=2, metavar='S',
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    # checking point
+
+
+    """ checking point """
     parser.add_argument('--resume', type=str, default=None,
                         help='put the path to resuming file if needed')
-    parser.add_argument('--saved-arch-path', type=str, default='searched_arch',
+    parser.add_argument('--saved-arch-path', type=str, default=None,
                         help='put the path to alphas and betas')
     parser.add_argument('--checkname', type=str, default=None,
                         help='set the checkpoint name')
-    # evaluation option
-    parser.add_argument('--filter_multiplier', type=int, default=20)
-    parser.add_argument('--autodeeplab', type=str, default='train',
-                        choices=['search', 'train'])
+
 
     args = parser.parse_args()
     args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -245,15 +243,20 @@ def main():
         except ValueError:
             raise ValueError('Argument --gpu_ids must be a comma-separated list of integers only')
 
+    if args.cuda and len(args.gpu_ids) > 1:
+        args.sync_bn = True
+    else:
+        args.sync_bn = False
+
     if args.checkname is None:
         args.checkname = 'evaluation'
     print(args)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
-    new_trainer = trainNew(args)
+    evaluation = Evaluation(args)
 
-    new_trainer.validation()
-    new_trainer.writer.close()
+    evaluation.validation()
+    evaluation.writer.close()
 
 if __name__ == "__main__":
    main()
